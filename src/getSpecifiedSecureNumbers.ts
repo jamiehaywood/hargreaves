1b9a99db18f5c8e83d663ca4c4471f5c27fd3510
Jamie Haywood <jamie.haywood@gmail.com>
2019-12-19 21:20:09 +0000

    added non-null assertion operator

--- src/getSpecifiedSecureNumbers.ts
+++ src/getSpecifiedSecureNumbers.ts
@@ -15,9 +15,9 @@ export default async function getSpecifiedSecureNumbers(secureNumber: string, co
                 case 200:
                     let array = secureNumber.split("")
                     const $ = cheerio.load(body)
-                    let s1 = parseInt($('input[id="secure-number-1"]').attr('title').match(/[0-9]/g)![0]) - 1
-                    let s2 = parseInt($('input[id="secure-number-2"]').attr('title').match(/[0-9]/g)![0]) - 1
-                    let s3 = parseInt($('input[id="secure-number-3"]').attr('title').match(/[0-9]/g)![0]) - 1
+                    let s1 = parseInt($('input[id="secure-number-1"]').attr('title')!.match(/[0-9]/g)![0]) - 1
+                    let s2 = parseInt($('input[id="secure-number-2"]').attr('title')!.match(/[0-9]/g)![0]) - 1
+                    let s3 = parseInt($('input[id="secure-number-3"]').attr('title')!.match(/[0-9]/g)![0]) - 1
                     let secureNumberPositions = []
                     secureNumberPositions.push(array[s1])
                     secureNumberPositions.push(array[s2])
