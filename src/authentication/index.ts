a98d06c6aaf2b1f6386b993a91a8f68bde250478
Jamie Haywood <jamie.haywood@gmail.com>
2019-12-31 13:52:11 +0000

    adjusted file structure and added index

---
+++ src/authentication/index.ts
@@ -0,0 +1,11 @@
+import getSpecifiedSecureNumbers from './getSpecifiedSecureNumbers'
+import getToken from './getToken'
+import postSecureNumberAndPass from './postSecureNumberAndPass'
+import postUserAndDob from './postUserAndDob'
+
+export {
+    getSpecifiedSecureNumbers,
+    getToken,
+    postSecureNumberAndPass,
+    postUserAndDob
+}
