35ae39b52f8657faa8250fd63fbd4ac4eb26df4f
Jamie Haywood <jamie.haywood@gmail.com>
2020-03-21 22:23:01 +0000

    added interfaces file

---
+++ src/interfaces.ts
@@ -0,0 +1,65 @@
+export interface IAccounts {
+    [key: string]: IAccount
+}
+
+export interface IAccount {
+    stockValue: string,
+    cashValue: string,
+    totalValue: string,
+    available: string,
+    holdings: IInvestments
+}
+
+export interface IInvestments {
+    [key: string]: IInvestment
+}
+
+export interface IInvestment {
+    [key: string]: any
+    stockType: string;
+    stockInfo: string;
+    stockLink?: string;
+    unitsHeld: string;
+    price: string;
+    value: string;
+    cost: string;
+    gain: string;
+    gainPercentage: string;
+    details?: IInvestmentDetails;
+    ticker: string;
+}
+
+export interface IInvestmentDetails {
+    [key: string]: any
+    sedol: string;
+    transactions: ITransactions
+    lastDividend: string;
+    nextDividend: string;
+    firstDeal: string;
+    lastDeal: string;
+    totalDeals: string;
+    holdingSummary?: IHoldingSummary
+}
+
+export interface IHoldingSummary {
+    [key: string]: any
+    highestPricePaid: string
+    lowestPricePaid: string
+    weightedAvgPaid: string
+    highestPriceRecieved: string
+    lowestPriceRecieved: string
+    weightedAvgRecieved: string
+}
+
+export interface ITransactions {
+    [key: string]: ITransaction
+}
+
+export interface ITransaction {
+    date: string;
+    type: string;
+    reference: string;
+    unitCost: number;
+    quantity: number;
+    cost: number;
+}
