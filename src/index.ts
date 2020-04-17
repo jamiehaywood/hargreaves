b4949ac5424eeec88f0e80a98854a9c211e69d07
Jamie Haywood <jamie.haywood@gmail.com>
2020-04-17 18:31:11 +0100

    renaming

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
-import { hl } from './hl'
-import * as express from 'express'
-import * as bodyParser from 'body-parser';
+import { HL } from "./hl";
+import * as express from "express";
+import * as bodyParser from "body-parser";
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', async function (req, res) {
-    let username = req.body.username
-    let password = req.body.password
-    let secureNumber = req.body.secureNumber
-    let dateOfBirth = req.body.dateOfBirth
+app.post("/", async function(req, res) {
+  let username = req.body.username;
+  let password = req.body.password;
+  let secureNumber = req.body.secureNumber;
+  let dateOfBirth = req.body.dateOfBirth;
 
-    let instance = new hl()
-    await instance.authenticate(username, password, dateOfBirth, secureNumber)
-    res.send(await instance.getInDepth())
-})
+  let hl = new HL();
+  await hl.authenticate(username, password, dateOfBirth, secureNumber);
+  res.send(await hl.getInDepth());
+});
 
-
-app.listen(port, () => console.log(`HL API listening on port ${port}!`))
+app.listen(port, () => console.log(`HL API listening on port ${port}!`));
