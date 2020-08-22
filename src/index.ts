1e0d1789751634a0894549b4eb09946758e7a35d
Jamie Haywood <jamie.haywood@gmail.com>
2020-08-23 00:46:24 +0100

    refactor: rename `hl.ts` to `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
 import request from "./requestInstance";
-import { parseSecurityToken } from "./utils/parseSecurityToken";
-import { postUsernameDob } from "./authentication/postUsernameDob";
-import { parseSecureNumbers } from "./utils/parseSecureNumbers";
-import { postSecureNumbers } from "./authentication/postSecureNumbers";
+import { parseSecurityToken, parseSecureNumbers } from "./utils";
+import { postUsernameDob, postSecureNumbers } from "./authentication";
 import { AccountsCollection } from "./AccountsCollection";
 import { clearCookieJar } from "./requestInstance";
 // import { postSecureNumbers } from "./authentication/postSecureNumbers";
-
+interface Credentials {
+  username: string;
+  password: string;
+  dateOfBirth: string;
+  secureNumber: string;
+}
 export default class HL {
-  
   /**
-   * This method authenticates the HL class instance with Hargreaves Lansdown using credentials supplied
-   * @param {string} username Hargreaves Lansdown username.
-   * @param {string} password Hargreaves Lansdown password.
-   * @param {string} dateOfBirth Date of Birth.
-   * @param {string} secureNumber Hargreaves Lansdown secure number.
+   * @param {Object} credentials This is a credentials object used to authenticate the HL instance.
+   * @param {string} credentials.username Your Hargreaves Lansdown username
+   * @param {string} credentials.password Your Hargreaves Lansdown password
+   * @param {string} credentials.dateOfBirth The date of birth associated with your HL account
+   * @param {string} credentials.secureNumber The secure number associated with your HL account
    */
-
-  async authenticate(
-    username: string,
-    password: string,
-    dateOfBirth: string,
-    secureNumber: string
-  ) {
+  async authenticate({
+    username,
+    password,
+    dateOfBirth,
+    secureNumber,
+  }: Credentials) {
     let stageOneHtml = (
       await request.get("https://online.hl.co.uk/my-accounts/login-step-one")
     ).body;
